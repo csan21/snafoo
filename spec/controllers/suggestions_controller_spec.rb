716e3b166415371f8b5a350f02b5e3a3ba5b75ad
csan21 <chrisswho@gmail.com>
2017-04-27 13:26:44 -0500

    Add controller test gem to gemfile and write first controller test

--- spec/controllers/suggestions_controller_spec.rb
+++ spec/controllers/suggestions_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe SuggestionsController, type: :controller do
 
+  describe 'homepage action' do
+
+    it 'should load suggestions index as homepage' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
 end
