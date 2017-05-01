127deb8a156346f9e2a7f2924f54eeae9b89d2d5
csan21 <chrisswho@gmail.com>
2017-05-01 15:01:25 -0500

    Fix suggestion name validation

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,4 +2,6 @@ class Suggestion < ApplicationRecord
   has_many :votes
 
   validates_presence_of :name, :purchase_location
+  # suggestions cant be the same name regardless of letter casing
+  validates :name, uniqueness: { case_sensitive: false }
 end
