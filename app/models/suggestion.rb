d72cd86adf07fa9c5d59507f70dc822dfe10542e
csan21 <chrisswho@gmail.com>
2017-05-03 16:20:01 -0500

    Remove redundancy

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -5,12 +5,12 @@ class Suggestion < ApplicationRecord
 
   # list of snacks that are always purchased
   def self.purchased_list
-    @purchased = Suggestion.all.select { |snack| snack[:optional] == false }
+    Suggestion.all.select { |snack| snack[:optional] == false }
   end
 
   # list of snacks that are newly suggested
   def self.suggested_list
-    @suggested = Suggestion.all.select { |snack| snack[:optional] == true }
+    Suggestion.all.select { |snack| snack[:optional] == true }
   end
 
 end
